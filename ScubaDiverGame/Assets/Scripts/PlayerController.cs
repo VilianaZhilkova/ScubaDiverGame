17e623ded5c8eadd09be7995b6d718b62f5d950e
Milena Sapunova <milena.sapunova@gmail.com>
2016-06-23 20:26:54 +0300

    Flip player and add some bubbles

--- ScubaDiverGame/Assets/Scripts/PlayerController.cs
+++ ScubaDiverGame/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,9 @@ using Assets.Scripts.Interfaces;
 
 public class PlayerController : MonoBehaviour, IPlayer, IGameObject
 {
-    //public
-    public float movementSpeed = 1;
+    //effectively public
+    [SerializeField]
+    private float movementSpeed = 1;
     //public int jumpLenght;
     //private
     //private Rigidbody2D rb;
@@ -15,26 +16,34 @@ public class PlayerController : MonoBehaviour, IPlayer, IGameObject
     private float minX = -3.9f;
     private float maxY = 3.1f;
     private float minY = -3.1f;
+    private bool facingRight;
 
     public void Start()
     {
         //this.rb = this.GetComponent<Rigidbody2D>();
+        facingRight = true;
     }
 
     //read input / change graphics
     public void Update()
     {
-        Move(movementSpeed);
+        float[] horizontalAndVertical = GetHorizontalAndVertival();
+        Move(movementSpeed, horizontalAndVertical[0], horizontalAndVertical[1]);
+        Flip(horizontalAndVertical[0]);
     }
 
     //Methods
     public void Move(float speed)
     {
+
+    }
+
+    private void Move(float speed, float horizontal, float vertical)
+    {
         //this.isClicked = Input.GetButtonDown("Fire1");
 
-        this.transform.Translate(speed * Time.deltaTime * Input.GetAxis("Horizontal"), 0, 0);
-        this.transform.Translate(0, speed * Time.deltaTime * Input.GetAxis("Vertical"), 0);
-        
+        this.transform.Translate(speed * Time.deltaTime * horizontal, 0, 0);
+        this.transform.Translate(0, speed * Time.deltaTime * vertical, 0);
 
         if (this.transform.position.x > maxX)
         {
@@ -49,10 +58,28 @@ public class PlayerController : MonoBehaviour, IPlayer, IGameObject
         {
             this.transform.position = new Vector3(transform.position.x, maxY, transform.position.z);
         }
-        else if(this.transform.position.y < minY)
+        else if (this.transform.position.y < minY)
         {
             this.transform.position = new Vector3(transform.position.x, minY, transform.position.z);
         }
+    }
+
+    private void Flip(float horizontal)
+    {
+        if ((horizontal > 0 && !facingRight) || (horizontal < 0 && facingRight))
+        {
+            facingRight = !facingRight;
+            Vector3 scale = this.transform.localScale;
+            scale.x *= -1;
+            this.transform.localScale = scale;
+        }
+    }
+
+    private float[] GetHorizontalAndVertival()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        return new float[] { horizontal, vertical };
     }
 
     //apply physics
