224e27612bfce1216e3c39d0f38878b918d92bb1
DragomirPetrov <petrov.dragomir@yahoo.com>
2016-07-01 22:38:35 +0300

    added some new features

---
+++ ScubaDiver_2.0/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public Vector2 speed = new Vector2(10, 10);
+    public Vector2 direction = new Vector2(-1, 0);
+
+    // Update is called once per frame
+    void Update () {
+
+        Vector3 movement = new Vector3(speed.x * direction.x, speed.y * direction.y, 0);
+
+        movement *= Time.deltaTime; // to smooth out the movement on the screen
+
+        transform.Translate(movement); //moving the sprite
+
+
+    }
+}
