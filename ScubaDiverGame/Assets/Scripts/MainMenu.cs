def05977e43ef3be8a8bbf59ce197f3d826bacf9
Milen Tanev <mil3ntanev@abv.bg>
2016-07-06 10:47:03 -0700

    Add files via upload
    Скрипт към главното меню. Бутоните за спиране на звуците все още нищо не правят, освен да принтират в конзолата, но все още нямаме звуци, така че е допустимо :)

---
+++ ScubaDiverGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayButton()
+    {
+        SceneManager.LoadScene("ScubaDiverGame");
+    }
+
+    public void MuteMusic()
+    {
+        Debug.Log("Music muted");
+    }
+
+    public void MuteSounds()
+    {
+        Debug.Log("Sounds muted");
+    }
+}
