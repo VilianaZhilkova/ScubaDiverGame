d7e85b273b30222f09174af2047f444cfd304778
DilyanaKom <dilyana.komitova@gmail.com>
2016-07-09 15:26:28 +0300

    add some classes for the health bar

---
+++ ScubaDiverGame/Assets/Scripts/HealthBar/PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+    [SerializeField]
+    private Stat health;
+
+    private void Awake()
+    {
+        health.Initialize();
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            health.CurrentValue -= 2;
+        }
+	}
+}
