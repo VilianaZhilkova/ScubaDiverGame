f960503b33333c2bf4131b4a9f2cd5ac18439845
Viliana Zhilkova <vilianazhilkova@abv.bg>
2016-07-11 22:05:26 +0300

    ClassDiagram image again

---
+++ ScubaDiverGame/Assets/Scripts/Enumerations/FishesMoveSpeed.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Enumerations
+{
+    public enum FishesMoveSpeed
+    {
+        BoneFishMoveSpeed = 8, //0.08f
+        SquidMoveSpeed = 1, //0.01f;
+        UrchinFishMoveSpeed = 4, //0.04f;
+        FishDefautMoveSpeed = 9 //0.09f;
+    }
+}
